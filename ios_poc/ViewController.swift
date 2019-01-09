5f5f787df2884150a9adb8e60813894c7098c250
Korakot Phaearaya <korakop.p@gmail.com>
2019-01-09 09:39:20 +0700

    init

---
+++ ios_poc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_poc
+//
+//  Created by korakot on 9/1/2562 BE.
+//  Copyright © 2562 korakot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios_pocTests/ios_pocTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_pocTests.swift
+//  ios_pocTests
+//
+//  Created by korakot on 9/1/2562 BE.
+//  Copyright © 2562 korakot. All rights reserved.
+//
+
+import XCTest
+@testable import ios_poc
+
+class ios_pocTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
